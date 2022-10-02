8bcb4852e0e7894ce15a2ebcdd97cf92abac0d0c
Rashmigautam16 <rashmigaut99@gmail.com>
2022-10-02 18:49:44 +0530

    marge

--- confused-volcano/src/Header/Sidenav.jsx
+++ confused-volcano/src/Header/Sidenav.jsx
@@ -33,10 +33,10 @@ export default function SizeExample() {
         <DrawerContent>
           <DrawerCloseButton w="50px" pr="20%"  mt="20px" fontSize="20px" ml="30px" border="none" bg="#0087A8" color="white"/>
           <DrawerHeader  bg="#0087A8" w="20%"  color="white" pt="50px"    fontSize="18px"  fontWeight="700" ></DrawerHeader>
-          <DrawerBody bg="#0087A8" w="20%"  color="white" fontSize="16px" fontWeight="700" height="1000px" >
+          <DrawerBody bg="#0087A8" w="20%"  color="white" fontSize="16px" fontWeight="700" height="900px" >
 
             <p style={padding}>BENGALURU <Sidedropdown/><hr color="#d8e2d2"/></p>
-            
+
             <p style={padding}>KARNATAKA <Sidedropdown/><hr color="#d8e2d2"/></p>
             <p style={padding}>NATIONAL  <Sidedropdown/><hr color="#d8e2d2"/></p>
             <p style={padding}>SPORTS    <Sidedropdown/><hr color="#d8e2d2"/></p>
